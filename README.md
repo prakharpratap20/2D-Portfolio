45141862c6cf6739930503a3213c922d6d9cfc58
Prakhar Pratap <prakharpratap2001@gmail.com>
2024-04-06 23:04:01 +0530

    will add more functionality

---
+++ README.md
@@ -0,0 +1,20 @@
+# Developer portfolio It's a 2D game
+
+# How to run
+
+Note: You need `Node.js` and `npm` installed on your machine.
+
+`npm install` then `npm run dev`
+
+# How to build
+
+`npm run build` and a dist folder should be created.
+
+# How to preview the build
+
+`npm run preview`
+
+# How to host?
+
+Once the project is built. Take the content of the dist folder and drag and drop it
+to your favorite static site hosting provider.
---
+++ src/constants.js
@@ -0,0 +1,18 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in JavaScript/Python these days.
+    I also like C/C++. Anyway regardless of the language, I just like programming and solving problems.
+    Here is my <a href="https://github.com/prakharpratap20" target="_blank">Github</a>!`,
+  "cs-degree": `This is my CS degree. `,
+  "sofa-table": `That's my sofa. I like to relax here and watch Netflix. `,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+   <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a> and <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a>!`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="https://github.com/prakharpratap20/Resume/tree/main/resume.pdf" target="_blank">Check it out?</a>
+  Contact me at prakharpratap2001@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+  library: `There are a lot of programming books on my shelves. I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
+  exit: `If you want to exit Prakhar's portfolio, just close the tab.`,
+};
